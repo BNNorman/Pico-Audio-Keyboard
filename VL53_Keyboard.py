9cc5f7aaf4bef74ca1f52df66293663c1404d4cb
Brian <brian.n.norman@gmail.com>
2025-02-05 14:46:58 +0000

    Update VL53_Keyboard.py
    Tidy up some bits

--- VL53_Keyboard.py
+++ VL53_Keyboard.py
@@ -1,6 +1,8 @@
 '''
 VL53_Keyboard
 
+Normalises the key readings by dynamically adjusting the min/max
+
 
 '''
 from adafruit_tca9548a import TCA9548A,TCA9548A_Channel
@@ -8,7 +10,7 @@ from adafruit_vl53l0x import VL53L0X
 import busio
 import board
 from digitalio import DigitalInOut,Direction,Pull
-
+import sys
 
 NUM_KEYS=8 	# also number of channels on the MUX
 
@@ -21,47 +23,40 @@ class Keyboard():
         self.reset_pin.value=1 # Low to reset
 
         # as keys are read the min/max readings are updated
-        # so that a 0..1 range can be calculated
-        self.minLevel=[20]*NUM_KEYS	 # empirically determined min VL53 
-        self.maxLevel=[0]*NUM_KEYS  # ditto but max VL53
+        # so that a 0..1 range can be calculated later
+        self.minLevel=[1000]*NUM_KEYS	 # adjusted when keys are read
+        self.maxLevel=[0]*NUM_KEYS       # ditto
         
-        self.cache=[0.0]*NUM_KEYS 	 # last value used if not data_ready
+        # last valid reading
+        # if not data_ready then this value is used
+        # updated as keys are read
+        self.cache=[0.0]*NUM_KEYS 
 
         # create the mux
         try:
             self.i2c=busio.I2C(SCL,SDA) # MUX
+            self.mux=TCA9548A(self.i2c) # using default MUX address 0x70
         except Exception as e:
-            exit(f"EXCEPTION: Unable to setup the MIDI Keyboard")
-            
-        self.mux=TCA9548A(self.i2c) # using default MUX address 0x70
+            sys.exit(f"EXCEPTION: Unable to setup the MIDI Keyboard")
 
         # associate the 'key' sensors to the device channels
         self.tsl=[None]*NUM_KEYS
         for ch in range(NUM_KEYS):
             self.tsl[ch]=VL53L0X(self.mux[ch])
+            # all the sensors run in parallel
             self.tsl[ch].start_continuous()
             self.tsl[ch].measurement_timing_budget = 33000 # us 
             #self.tsl[ch].io_timeout_s=0.02 # 2x timing budget
 
-    def __del__(self):
-        self.dumpRanges()
-
-    def scanKeys(self):
+    def scanChannels(self):
         # check for sensors on each mux port
-        for channel in range(NUM_KEYS):
-            if self.mux[channel].try_lock():
-                print("Channel {}:".format(channel), end="")
+        for ch in range(NUM_KEYS):
+            if self.mux[ch].try_lock():
+                print(f"Ch {ch}, end=''")
                 addresses = self.mux[channel].scan()
                 print([hex(address) for address in addresses if address != 0x70])
                 self.mux[channel].unlock()
                 
-    def scanChannels(self):
-        for ch in range(NUM_KEYS):
-            print("CH",ch,end="")
-            while tch.try_lock():
-                found=self.mux.scan()
-            tch.unlock()
-            print("found",found)
     
     def normalise(self,ch,value):
         # map value to 0..1 range
@@ -78,9 +73,12 @@ class Keyboard():
             self.maxLevel[ch]=value
 
         # return normalised value
+
         range=self.maxLevel[ch]-self.minLevel[ch]
-        return round((value-self.minLevel[ch])/range,2)
-        
+        if range>0:
+            return round((value-self.minLevel[ch])/range,2)
+        # (val-min)/range would be an infinite value
+        return self.maxLevel[ch]
         
     def getAllLevels(self):
         # read all the sensors and return a list of readings
@@ -128,20 +126,16 @@ if __name__=="__main__":
     import time
     
     print("Scanning channels/keys")
-    #kbd.scanKeys()
+    #kbd.scanChannels()
     
     try:
         count=0
         while True:
-            print()
             lev=kbd.getAllLevels()
-            for l in lev:
-                print(l," ",end="")
-            print()
-            #kbd.dumpRanges()
-            time.sleep(2)
+            print(f"{count} {lev}")
+            time.sleep(.2)
             count+=1
-            print("Count",count)
+            
     except Exception as e:
         print("EXCEPTION ",e)
         kbd.dumpRanges()
